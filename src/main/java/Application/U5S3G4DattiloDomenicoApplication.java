a96a1d0fa4d8378378d53e73e6e02a2e56f90426
domenico2003 <domdat03@gmail.com>
2023-06-15 15:06:27 +0200

    configurazione iniziale progetto

---
+++ src/main/java/Application/U5S3G4DattiloDomenicoApplication.java
@@ -0,0 +1,13 @@
+package Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5S3G4DattiloDomenicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5S3G4DattiloDomenicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Application/U5S3G4DattiloDomenicoApplicationTests.java
@@ -0,0 +1,13 @@
+package Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5S3G4DattiloDomenicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
